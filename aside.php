61239af7f5ac91996c3c2ebf4ca46118e8b50a27
wesleymontaigne <wesleymontaigne@hotmail.com>
2020-02-03 19:44:14 -0200

    aside.php

---
+++ aside.php
@@ -0,0 +1,11 @@
+<h2>Real-time Auction Info</h2>
+<br>
+<br>
+<br>
+<h3>Bidder count: </h3><span>id="biddercount"</span> 
+<br><br>
+<h3>Item count: </h3><span>id"itemcount"</span>
+<br><br>
+<h3>Items total value:</h3><span>id="itemtotal"</span>
+<br><br>
+<h3>Bids totals: </h3><span> id="bidtotal"</span>
---
+++ nav.inc.php
@@ -0,0 +1,23 @@
+<table width=100% cellpadding="3">
+<tr>
+<?php
+if(!isset($_SESSION['login'])
+	echo "<td></td>\n";
+	else{
+
+
+
+
+?>
+</tr>
+
+<tr>
+<td><a href="index.php"><strong></strong></a> </td>
+</tr>
+
+
+<?php
+
+}
+?>
+</table>
